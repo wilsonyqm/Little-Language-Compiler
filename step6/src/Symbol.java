d3b756aed3d9b02e9b66aca7957fced573897c8e
Nelson0409 <dmengdi@gmail.com>
2015-11-17 22:11:39 -0500

    add symbol attr

--- step6/src/Symbol.java
+++ step6/src/Symbol.java
@@ -3,17 +3,28 @@ public class Symbol {
 	private String name;
 	private String type;
 	private String value;
+	private String attr;
 	
 	public Symbol(String name, String type){
 		this.name = name;
 		this.type = type;
 		this.value = null;
+		this.attr = null;
 	}
 	
-	public Symbol(String name, String type, String value){
+	public Symbol(String name, String type,String attr){
+		this.name = name;
+		this.type = type;
+		this.value = null;
+		this.attr = attr;
+	}
+	
+	
+	public Symbol(String name, String type, String value, String attr){
 		this.name = name;
 		this.type = type;
 		this.value = value;
+		this.attr = attr;
 	}
 	
 	public String getName(){
--- step6/src/function.java
+++ step6/src/function.java
@@ -2,17 +2,35 @@ import java.util.*;
 
 public class Function{
 	CodeGenerater codeGenerater;
-	SymbolTable symboltable;
+	SymbolTable symbolTable;
 	int paraNum;
 	int localVarNum;
+	int registernum;
 	
 	public function(SymbolTalbe table){
 		this.codeGenerater = new CodeGenerater();
-		this.SymbolTable = table;
-		
+		this.symbolTable = table;
+		this.registernum = 1;
 	}
 	
 	public CodeGenerater getCodeGenerater(){
 		return this.codeGenerater;
+	}
+	
+	public SymbolTable getTable(){
+		return this.symbolTable;
+	}
+	
+	private String getRegister(){
+			
+		return "$T"+registernum++; 
+	}
+	
+	private String getLocal(){
+		return "$L"+localVarNum++;
+	}
+	
+	private String getPara(){
+		return "$P"+paraNum++;
 	}
 }
